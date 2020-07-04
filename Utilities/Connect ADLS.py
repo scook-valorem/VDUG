fdc873851b95475b102e7ab8754b76f914462d8b
scook-valorem <48368304+scook-valorem@users.noreply.github.com>
2020-07-04 14:51:11 -0500

    Update: Episode 0.py Episode 1.py Episode 2.py Episode 3.py Episode 4.py Episode 5.py Episode 6.py README.md Stages Utilities VDUG Sessions notebooks

---
+++ Utilities/Connect ADLS.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+# MAGIC %run Utilities/parameters
+
+# COMMAND ----------
+
+spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "true")
+# use if you don't already have a container
+# dbutils.fs.ls("abfss://<file-system-name>@<storage-account-name>.dfs.core.windows.net/")
+spark.conf.set("fs.azure.createRemoteFiconfigs = {"fs.azure.account.auth.type": "OAuth",
+           "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+           "fs.azure.account.oauth2.client.id": "ba686cbc-b9bf-46be-8403-573c9501f298",
+           "fs.azure.account.oauth2.client.secret":,
+           "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/5c8085d9-1e88-4bb6-b5bd-e6e6d5b5babd/oauth2/token"}
+
+# Optionally, you can add <directory-name> to the source URI of your mount point.
+dbutils.fs.mount(
+  source = "abfss://default@mixerdemolake.dfs.core.windows.net/",
+  mount_point = "/mnt/default",
+  extra_configs = configs)leSystemDuringInitialization", "false")
+spark.conf.set(
+  base_path, storage_key
+  )
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+dbutils.fs.ls('/mnt')
+
+# COMMAND ----------
+
