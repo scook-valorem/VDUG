0299653053b6f84e33c5ef5fa4af1b4da44bdc52
scook-valorem <48368304+scook-valorem@users.noreply.github.com>
2020-08-26 12:42:35 -0500

    Bringing up to date

--- notebooks/Utilities/functions.py
+++ notebooks/Utilities/functions.py
@@ -29,3 +29,27 @@ def flatten_df(nested_df):
 
 # COMMAND ----------
 
+# add type hint for dataframe
+def write_delta_table(df, name: str ):
+  df.writeStream.format('delta').option('path',  '{}{}'.format(untappd_base_query_path,name)).option('checkpointLocation', untappd_base_query_path+'{}/checkpoints'.format(name)).outputMode("append").trigger(once=True).start()
+def register_delta_table(name: str):
+  spark.sql(
+  '''
+  CREATE TABLE IF NOT EXISTS {}
+  USING DELTA
+  LOCATION '{}'
+  '''.format(name, '{}{}'.format(untappd_base_query_path,name))
+  )
+
+# COMMAND ----------
+
+def clean_flat_column_names(df, col_name):
+  df_clean = df
+  for col in df.columns:
+    splits = col.split('{}_'.format(col_name))
+    name = splits[len(splits) - 1]
+    df_clean = df_clean.withColumnRenamed(col,name)
+  return df_clean
+
+# COMMAND ----------
+
