1250af2609974d016fb95b7de0305f26cba94c5a
scook-valorem <48368304+scook-valorem@users.noreply.github.com>
2020-11-11 12:37:07 -0600

    Adding nightly job notebook

---
+++ Nightly Job.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Run Bronze layer
+
+# COMMAND ----------
+
+dbutils.notebook.run('Stages/bronze',600, {"username":"slcc2c"})
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Run Silve layer
+
+# COMMAND ----------
+
+dbutils.notebook.run('Stages/silver',600)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Run Gold layer
+
+# COMMAND ----------
+
+dbutils.notebook.run('Stages/gold',600)
